c713d056b45ce91d60a1e65c6c5bb7432781f8dc
Abhishek K <kabhik2898@gmail.com>
2022-09-23 07:47:47 +0530

    Singleton class created

--- src/packageSingleton/Aadhaar.java
+++ src/packageSingleton/Aadhaar.java
@@ -1,5 +1,24 @@
 package packageSingleton;
 
 public class Aadhaar {
+	
+	private int adhaarNo;
+	
+	private Aadhaar ar;
+	
+	private Aadhaar(int adhaarNo) {
+		this.adhaarNo = adhaarNo;
+	}
+	
+	public Aadhaar createObj(int no) {
+		if(ar==null) {
+			ar = new Aadhaar(no);
+		}
+		else {
+			System.out.println("Aadhaar already created");
+			System.out.println("Your Aadhaar No: "+this.adhaarNo);
+		}
+		return ar;
+	}
 
 }
